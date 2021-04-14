f63a035afedea69eca17d33f67af14dcb2ec6392
Maciej Adamczyk <kontakt@maciejadamczyk.com.pl>
2021-04-14 12:08:15 +0200

    build: add client panel

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 $(document).ready(() => {
-    let root = $('#root');
     let play=new Play();
-    play.createDiscs(5);
     play.createTowers();
+    $('#btnStart').on('click',()=>{
+        const n=parseInt($('#number').val());
+        if(n<2) return;
+        $('#root').empty();
+        play=new Play();
+        play.n=n
+        play.start=true;
+        play.createTowers();
+        play.createDiscs();
+        $('#start').hide(1000)
+    });
 });
--- src/Play.js
+++ src/Play.js
@@ -1,17 +1,18 @@
 class Play{
     constructor() {
-        this.root=$('#root');
-        this.camera=new THREE.PerspectiveCamera(45,this.root.width()/this.root.height(),0.1,10000);
+        const root=$('#root');
+        this.camera=new THREE.PerspectiveCamera(45,root.width()/root.height(),0.1,10000);
         this.renderer=new THREE.WebGLRenderer();
         this.scene=new THREE.Scene();
         this.renderer.setClearColor(0x000000);
-        this.renderer.setSize(this.root.width(),this.root.height());
-        this.root.append(this.renderer.domElement);
-        this.camera.position.set(0,0,100);
+        this.renderer.setSize(root.width(),root.height());
+        root.append(this.renderer.domElement);
+        this.camera.position.set(0,0,0);
         this.camera.lookAt(0,0,0);
         this.discs=[];
         this.towers=[];
-        this.n=0;
+        this.n=5;
+        this.start=false;
         this.render();
     }
 
@@ -21,11 +22,10 @@ class Play{
         else return parseInt(color,16);
     }
 
-    createDiscs=n=>{
-        this.n=n;
-        const max=1+(n-1)*0.3;
-        for(let i=0;i<n;i++){
-            this.discs[i]=new Disc(-1*n*4,i-((n+5)/2)+1,0,max-i*0.3,this.randomColor());
+    createDiscs=()=>{
+        const max=1+(this.n-1)*0.3;
+        for(let i=0;i<this.n;i++){
+            this.discs[i]=new Disc(-1*this.n*4,i-((this.n+5)/2)+1,0,max-i*0.3,this.randomColor());
             this.scene.add(this.discs[i]);
         }
     }
@@ -35,10 +35,14 @@ class Play{
             this.towers[i+1]=new Tower(i*this.n*4,0,0,this.n,i+1);
             this.scene.add(this.towers[i+1]);
         }
-        this.camera.position.z=this.n*10;
     }
 
     render=()=>{
+        if(this.start){
+            if(this.camera.position.z<this.n*10) this.camera.position.z+=0.1*this.n;
+            else this.start=false;
+            this.camera.lookAt(0,0,0);
+        }
         requestAnimationFrame(this.render);
         this.renderer.render(this.scene,this.camera);
     }
