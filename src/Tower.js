b73e3acdf1e2696fef0b87dba7d1f68c2488efab
Maciej Adamczyk <kontakt@maciejadamczyk.com.pl>
2021-04-14 12:12:46 +0200

    fix: change size of tower

--- src/Tower.js
+++ src/Tower.js
@@ -2,7 +2,7 @@ class Tower extends THREE.Object3D{
     constructor(x,y,z,discs,number=0) {
         super();
         const material = new THREE.MeshBasicMaterial({color:0xaaaaaa});
-        const cylinderGeometry = new THREE.CylinderGeometry(1,1,discs+5,10);
+        const cylinderGeometry = new THREE.CylinderGeometry(0.1*discs,0.1*discs,discs+5,10);
         const baseGeometry=new THREE.CylinderGeometry(discs,discs,1,10);
         const tower=new THREE.Mesh(cylinderGeometry);
         const base=new THREE.Mesh(baseGeometry);
